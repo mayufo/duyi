974b6964398c87d8764c5a6b363cc15f2c834659
MFCDE <76235150@qq.com>
2018-06-18 22:16:29 +0800

    '6-18'

---
+++ jQuery/tab组件/tab.js
@@ -0,0 +1,24 @@
+function Tabs(selector) {
+    this.elements = $(selector);
+    this.init();
+    this.bindEvents();
+}
+
+Tabs.prototype.init = function () {
+    this.elements.each(function (index, elem) {
+        $(elem).children('.tab-bar').children('li').eq(0).addClass('active');
+        $(elem).children('.tab-content').children('li').eq(0).addClass('active');
+    });
+}
+
+Tabs.prototype.bindEvents = function () {
+    this.elements.on('click', '.tab-bar>li', function (e) {
+        // var $li = $(e.currentTarget);
+        $(this).addClass('active').siblings().removeClass('active');
+        var index = $(this).index();
+        $(this).closest('.tabs').find('.tab-content>li').eq(index).addClass('active').siblings().removeClass('active');
+    });
+}
+
+// var tabs = new Tabs('.tabs');
+var tabs = new Tabs('.tab1');
--- jQuery/淘宝轮播图/swiper/swiper.js
+++ jQuery/淘宝轮播图/swiper/swiper.js
@@ -1,5 +1,99 @@
 var swiper = {
+    $silderPage: $('.silderPage'),
+    moveWidth: $('.silderPage li').width(),
+    num: $('.silderPage li').length - 1,
+    nowIndex: 0,
+    flag: true,
+    silderTimer: null,
     init: function () {
-        this.bindEvent()
+        this.bindEvent();
+        // this.autoMove();
+    },
+    bindEvent: function () {
+        var that = this;
+        $('.prevBtn').add($('.nextBtn')).add($('.silderIndex li')).on('click', function () {
+            if ($(this).attr('class') == 'prevBtn') {
+                that.move('prev');
+            } else if ($(this).attr('class') == 'nextBtn') {
+                that.move('next');
+            } else {
+                that.move($(this).index());
+            }
+        });
+
+        $('.wrapper').on('mouseenter', function () {
+            clearTimeout(that.silderTimer);
+            $('.silderBtn').removeClass('hide').addClass('show');
+        }).on('mouseleave', function () {
+            that.autoMove();
+            $('.silderBtn').removeClass('show').addClass('hide');
+        });
+
+        $('.silderBtn').add($('.silderIndex ul')).on('mouseenter', function () {
+            that.autoMove();
+        });
+    },
+    move: function (direction) {
+        if (this.flag) {
+            this.flag = false;
+            var that = this;
+            var a = 1;
+            if (direction == 'prev' || direction == 'next') {
+                if (direction == 'prev') {
+                    if (this.nowIndex == 0) {
+                        this.$silderPage.css({
+                            left: -this.num * this.moveWidth
+                        });
+                        this.nowIndex = this.num - 1;
+                    } else {
+                        this.nowIndex--;
+                    }
+                } else {
+                    if (this.nowIndex == this.num - 1) {
+                        a = 0;
+                        this.$silderPage.animate({
+                            left: -this.num * this.moveWidth
+                        }, 150, function () {
+                            $(this).css({
+                                left: 0
+                            });
+                            that.flag = true;
+                            that.nowIndex = 0;
+                            that.autoMove();
+                        });
+                    } else {
+                        this.nowIndex++;
+                    }
+                }
+            } else {
+                this.nowIndex = direction;
+            }
+
+            this.changeIndexStyle(this.nowIndex);
+
+            if (a) {
+                this.$silderPage.animate({
+                    left: -this.nowIndex * this.moveWidth
+                }, 150, function () {
+                    that.flag = true;
+                    that.autoMove();
+                });
+            }
+        }
+
+    },
+    changeIndexStyle: function (index) {
+        $('.silderIndex .active').removeClass('active');
+        $('.silderIndex li').eq(index).addClass('active');
+    },
+    autoMove: function () {
+        var that = this;
+        clearTimeout(that.silderTimer);
+        this.silderTimer = setTimeout(function () {
+            that.move('next');
+        }, 1500);
     }
 }
+
+swiper.init();
+

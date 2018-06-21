29173e10c68d04329fbd9ec46e639239f920e6e7
wk <76235150@qq.com>
2018-06-21 18:15:27 +0800

    6-21

--- jQuery/tab组件/tab.js
+++ jQuery/tab组件/tab.js
@@ -1,24 +1,28 @@
-function Tabs(selector) {
-    this.elements = $(selector);
-    this.init();
-    this.bindEvents();
-}
+var Tabs = (function ($) {
+    function Tabs(selector) {
+        this.elements = $(selector);
+        this.init();
+        this.bindEvents();
+    }
 
-Tabs.prototype.init = function () {
-    this.elements.each(function (index, elem) {
-        $(elem).children('.tab-bar').children('li').eq(0).addClass('active');
-        $(elem).children('.tab-content').children('li').eq(0).addClass('active');
-    });
-}
+    Tabs.prototype.init = function () {
+        this.elements.each(function (index, elem) {
+            $(elem).children('.tab-bar').children('li').eq(0).addClass('active');
+            $(elem).children('.tab-content').children('li').eq(0).addClass('active');
+        });
+    }
 
-Tabs.prototype.bindEvents = function () {
-    this.elements.on('click', '.tab-bar>li', function (e) {
-        // var $li = $(e.currentTarget);
-        $(this).addClass('active').siblings().removeClass('active');
-        var index = $(this).index();
-        $(this).closest('.tabs').find('.tab-content>li').eq(index).addClass('active').siblings().removeClass('active');
-    });
-}
+    Tabs.prototype.bindEvents = function () {
+        this.elements.on('click', '.tab-bar>li', function (e) {
+            // var $li = $(e.currentTarget);
+            $(this).addClass('active').siblings().removeClass('active');
+            var index = $(this).index();
+            $(this).closest('.tabs').find('.tab-content>li').eq(index).addClass('active').siblings().removeClass('active');
+        });
+    }
+
+    return Tabs;
+})(jQuery);
 
 // var tabs = new Tabs('.tabs');
 var tabs = new Tabs('.tab1');
